6cde26720796901f0b99f5f8fffe2f070b54f9ca
santosm46 <santos46ma@gmail.com>
2019-10-07 22:29:56 -0300

    Criando menu.
    Signed-off-by: santosm46 <santos46ma@gmail.com>
    Co-authored-by: Lucas Lopes <l.lopes.fga@gmail.com>
    Co-authored-by: FabianaRibas <fabianalv.ribas@gmail.com>
    Co-authored-by: Matheus Clemente <matheus_klementt@hotmail.com>

---
+++ menu.pl
@@ -0,0 +1,33 @@
+:- [regras].
+
+% opt(1) :- nl, write('Tamagotchi tá nascendoo...'), nl, nl, sleep(1), X is random(4) + 1 ,  shell(clear), write('Seu pet é um '), raca(X), nl, sobre(X), nl,write('Dê um nome ao seu pet... '),nl,read(N), nl, interagir(N, X),nl.
+
+% opt(0) :- nl, write('Tamagotchi se foi...'), nl, nl,halt.
+
+run :- read(X).
+% , opt(X).
+
+menu :-
+    write(' _____                                 _       _     _' ), nl,
+    write('|_   _|_ _ _ __ ___   __ _  __ _  ___ | |_ ___| |__ (_)'), nl,
+    write('  | |/ _` | \'_ ` _ \\ / _` |/ _` |/ _ \\| __/ __| \'_ \\| |'), nl,
+    write('  | | (_| | | | | | | (_| | (_| | (_) | || (__| | | | |'), nl,
+    write('  |_|\\__,_|_| |_| |_|\\__,_|\\__, |\\___/ \\__\\___|_| |_|_|'), nl,
+    write('                           |___/'                       ), nl,
+    write(' ____                                '), nl,
+    write('| __ ) _ __ __ _ _____   _  ___ __ _ '), nl,
+    write('|  _ \\| \'__/ _` |_  / | | |/ __/ _` |'), nl,
+    write('| |_) | | | (_| |/ /| |_| | (_| (_| |'), nl,
+    write('|____/|_|  \\__,_/___|\\__,_|\\___\\__,_|'), nl, nl.
+
+    % write('1 - Começar'), nl,
+    % write('0 -  Sair'), nl, nl, read(Op),nl,nl,write('você escolheu :' ),write(Op),nl,
+
+    % opt(Op),nl,sleep(3).
+
+
+:- initialization main, halt.
+
+main :-
+
+    shell(clear), menu, run.
