0fe0982748deaab5ef6b2c6d65d0f1d56170093f
Lucas Lopes <l.lopes.fga@gmail.com>
2019-10-10 16:02:15 -0300

    Adiciona regra darBronca
    Co-authored-by: FabianaRibas <fabianalv.ribas@gmail.com>
    Signed-off-by: Lucas Lopes <l.lopes.fga@gmail.com>

--- regras.pl
+++ regras.pl
@@ -11,3 +11,4 @@ fazerCarinho(A) :-  carinho(A,sim),nl, write('To feliz mas to com fome!'),nl,nl;
 levarParaPassear(A):- passeio(A,sim),nl, write('To feliz mas to morrendo de fome!'),nl,nl;  write('ele não gostaria de sair '),nl,nl.
 darAlimento(A):- nl,write('Qual o alimento'),nl,read(B),alimento(A,B),nl, write('To muito feliz'), nl, nl;write('não é recomendado alimentar com isso'),nl,nl.
 atoDeIgnorar(A):- nl,ignora(A,sim), write('Estou inconsolável! :('),nl,nl; write('você não gostaria de receber uma também, né'),nl,nl.
+darBronca(A):- nl,bronca(A,sim),  write('Estou muito triste e arrependido! :('),nl,nl; write('ele pode reagir de forma ríspida'),nl,nl.
