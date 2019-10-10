cea35110487893c5aeb9cb6d077303a815a883ad
Lucas Lopes <l.lopes.fga@gmail.com>
2019-10-10 16:01:08 -0300

    Adiciona regras
    Co-authored-by: Marcelo Araujo <santos46ma@gmail.com>
    Signed-off-by: Lucas Lopes <l.lopes.fga@gmail.com>

--- regras.pl
+++ regras.pl
@@ -1,4 +1,3 @@
-
 :- consult('animais').
 :- ['caracteristicas'].
 especie(ID, Especie) :- animal(ID, Especie).
@@ -10,3 +9,5 @@ alimentar(Alimento, X,Lista):- findall(Alimento, alimentos(X,Alimento),Lista).
 
 fazerCarinho(A) :-  carinho(A,sim),nl, write('To feliz mas to com fome!'),nl,nl; write('não é bom tentar fazer carinho'), nl, nl.
 levarParaPassear(A):- passeio(A,sim),nl, write('To feliz mas to morrendo de fome!'),nl,nl;  write('ele não gostaria de sair '),nl,nl.
+darAlimento(A):- nl,write('Qual o alimento'),nl,read(B),alimento(A,B),nl, write('To muito feliz'), nl, nl;write('não é recomendado alimentar com isso'),nl,nl.
+atoDeIgnorar(A):- nl,ignora(A,sim), write('Estou inconsolável! :('),nl,nl; write('você não gostaria de receber uma também, né'),nl,nl.
