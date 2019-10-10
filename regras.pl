051ebcfb21f817d53c530fd3af5ec470b1db649a
Lucas Lopes <l.lopes.fga@gmail.com>
2019-10-10 12:37:25 -0300

    Adiciona regra fazerCarinho
    Co-authored-by: Matheus Clemente <matheus_klementt@hotmail.com>
    Signed-off-by: Lucas Lopes <l.lopes.fga@gmail.com>

--- regras.pl
+++ regras.pl
@@ -1,5 +1,12 @@
 
 :- consult('animais').
+:- ['caracteristicas'].
 especie(ID, Especie) :- animal(ID, Especie).
 adicionarAnimal(I,E):-assert(animal(I,E)).
 pegarEspecie(A) :- especie(A,E), write(E),nl.
+
+
+alimentar(Alimento, X,Lista):- findall(Alimento, alimentos(X,Alimento),Lista).
+
+fazerCarinho(A) :-  carinho(A,sim),nl, write('To feliz mas to com fome!'),nl,nl; write('não é bom tentar fazer carinho'), nl, nl.
+% sentimento(1,A) : fazerCarinho(A).
