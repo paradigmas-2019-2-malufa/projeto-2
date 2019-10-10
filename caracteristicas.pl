8cc354f83627ce165150f1eb659c6e73d6201173
Lucas Lopes <l.lopes.fga@gmail.com>
2019-10-10 12:42:49 -0300

    Adiciona regra levarParaPassear
    Co-authored-by: Marcelo Araujo <santos46ma@gmail.com>
    Signed-off-by: Lucas Lopes <l.lopes.fga@gmail.com>

---
+++ caracteristicas.pl
@@ -0,0 +1,51 @@
+:-dynamic alimento/1.
+alimento(1,formigas).
+alimento(1,cupins).
+alimento(1,centopeias).
+alimento(1,vermes).
+
+
+alimento(2,sementes).
+alimento(2,frutinhas).
+alimento(2,ovos).
+alimento(2,insetos).
+alimento(2,pererecas).
+
+alimento(3,gamba).
+alimento(3,roedor).
+alimento(3,ave).
+alimento(3,lagarto).
+alimento(3,cobra).
+alimento(3,inseto).
+
+alimento(4,rato).
+alimento(4,gamba).
+alimento(4,roedor).
+alimento(4,peixe).
+alimento(4,ovos).
+alimento(4,cobra).
+
+:-dynamic carinho/1.
+carinho(1,sim).
+carinho(2,sim).
+carinho(3,nao).
+carinho(4,nao).
+
+:-dynamic passeio/1.
+passeio(1,sim).
+passeio(2,nao).
+passeio(3,nao).
+passeio(4,nao).
+
+
+:-dynamic bronca/1.
+bronca(1,nao).
+bronca(2,sim).
+bronca(3,nao).
+bronca(4,nao).
+
+:-dynamic ignora/1.
+ignora(1,nao).
+ignora(2,sim).
+ignora(3,nao).
+ignora(4,sim).
--- regras.pl
+++ regras.pl
@@ -9,4 +9,4 @@ pegarEspecie(A) :- especie(A,E), write(E),nl.
 alimentar(Alimento, X,Lista):- findall(Alimento, alimentos(X,Alimento),Lista).
 
 fazerCarinho(A) :-  carinho(A,sim),nl, write('To feliz mas to com fome!'),nl,nl; write('não é bom tentar fazer carinho'), nl, nl.
-% sentimento(1,A) : fazerCarinho(A).
+levarParaPassear(A):- passeio(A,sim),nl, write('To feliz mas to morrendo de fome!'),nl,nl;  write('ele não gostaria de sair '),nl,nl.
