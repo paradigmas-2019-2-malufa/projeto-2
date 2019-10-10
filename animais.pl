8d7778258ce82dbbd92cd49585b758c606064087
Lucas Lopes <l.lopes.fga@gmail.com>
2019-10-10 16:03:14 -0300

    Adiciona funcoes para exibir animais
    Co-authored-by: FabianaRibas <fabianalv.ribas@gmail.com>
    Signed-off-by: Lucas Lopes <l.lopes.fga@gmail.com>

--- animais.pl
+++ animais.pl
@@ -1,6 +1,23 @@
+:-['anta'].
+:-['jaguatirica'].
+:-['lobo-guara'].
+:-['papagaio'].
+:-['tatu-canastra'].
+:-['tamandua'].
+:-['tucano'].
+
 % animal(ID, Especie, ...)
 :-dynamic animal/2.
-animal(1, 'tamanduá-bandeira').
+animal(1, 'tamanduá').
 animal(2, 'tucano').
 animal(3, 'lobo-guara').
 animal(4, 'jaguatirica').
+
+:- dynamic exibir/1.
+exibir(1):- tamandua.
+exibir(2):- tucano.
+exibir(3):- lobo.
+exibir(4):- jaguatirica.
+exibir(5):- tatu.
+exibir(6):- tamandua.
+exibir(7):- tucano.
