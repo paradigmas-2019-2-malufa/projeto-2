7c040f7984247c8ceff2699122feb2335e9ca273
santosm46 <santos46ma@gmail.com>
2019-10-11 13:32:30 -0300

    Integrando imagens dos animais.
    Signed-off-by: matheusclemente <matheus_klementt@hotmail.com>
    Co-authored-by: Marcelo Araujo <santos46ma@gmail.com>

--- animais.pl
+++ animais.pl
@@ -1,13 +1,8 @@
-:-['anta'].
-:-['jaguatirica'].
-:-['lobo-guara'].
-:-['papagaio'].
-:-['tatu-canastra'].
-:-['tamandua'].
-:-['tucano'].
+:-['imagensAnimais'].
+
+:-dynamic animal/2.
 
 % animal(ID, Especie, ...)
-:-dynamic animal/2.
 animal(1, 'tamanduá').
 animal(2, 'tucano').
 animal(3, 'lobo-guara').
@@ -15,13 +10,3 @@ animal(4, 'jaguatirica').
 animal(5, 'anta').
 animal(6, 'tatu-canastra').
 animal(7, 'papagaio').
-
-:- dynamic exibir/1.
-exibir(1):- tamandua.
-exibir(2):- tucano.
-exibir(3):- lobo.
-exibir(4):- jaguatirica.
-exibir(5):- anta.
-exibir(6):- tatu.
-exibir(7):- papagaio.
-
